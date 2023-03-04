8493930bb1b6a345215d28bd600a412b4bd7b6c4
Roberto Mora <romogo17@gmail.com>
2023-03-04 14:40:52 +0000

    instrumented notifications_activities

--- backend-flask/services/notifications_activities.py
+++ backend-flask/services/notifications_activities.py
@@ -1,26 +1,36 @@
 from datetime import datetime, timedelta, timezone
+
+from opentelemetry import trace
+
+tracer = trace.get_tracer("notifications.activities")
+
 class NotificationsActivities:
   def run():
-    now = datetime.now(timezone.utc).astimezone()
-    results = [{
-      'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
-      'handle':  'coco',
-      'message': 'I am silver-colored unicorn',
-      'created_at': (now - timedelta(days=2)).isoformat(),
-      'expires_at': (now + timedelta(days=5)).isoformat(),
-      'likes_count': 5,
-      'replies_count': 1,
-      'reposts_count': 0,
-      'replies': [{
-        'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea66',
-        'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eae',
-        'handle':  'worf',
-        'message': 'this post has no honor!',
-        'likes_count': 0,
-        'replies_count': 0,
+    with tracer.start_as_current_span("notifications-activities-mock-data"):
+      now = datetime.now(timezone.utc).astimezone()
+
+      span = trace.get_current_span()
+      span.set_attribute("app.now", now.isoformat())
+
+      results = [{
+        'uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eee',
+        'handle':  'coco',
+        'message': 'I am silver-colored unicorn',
+        'created_at': (now - timedelta(days=2)).isoformat(),
+        'expires_at': (now + timedelta(days=5)).isoformat(),
+        'likes_count': 5,
+        'replies_count': 1,
         'reposts_count': 0,
-        'created_at': (now - timedelta(days=2)).isoformat()
-      }],
-    }
-    ]
-    return results
+        'replies': [{
+          'uuid': '26e12864-1c26-5c3a-9658-97a10f8fea66',
+          'reply_to_activity_uuid': '68f126b0-1ceb-4a33-88be-d90fa7109eae',
+          'handle':  'worf',
+          'message': 'this post has no honor!',
+          'likes_count': 0,
+          'replies_count': 0,
+          'reposts_count': 0,
+          'created_at': (now - timedelta(days=2)).isoformat()
+        }],
+      }
+      ]
+      return results
