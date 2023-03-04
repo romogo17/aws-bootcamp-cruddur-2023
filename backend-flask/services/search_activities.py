d5e3408eed2f1060e35d7087338ea7dd03987aef
Roberto Mora <romogo17@gmail.com>
2023-03-04 14:42:00 +0000

    instrumented search_activities

--- backend-flask/services/search_activities.py
+++ backend-flask/services/search_activities.py
@@ -1,21 +1,29 @@
 from datetime import datetime, timedelta, timezone
+
+from opentelemetry import trace
+
+tracer = trace.get_tracer("search.activities")
+
 class SearchActivities:
   def run(search_term):
-    model = {
-      'errors': None,
-      'data': None
-    }
+    with tracer.start_as_current_span("search-activities-mock-data"):
+      model = {
+        'errors': None,
+        'data': None
+      }
 
-    now = datetime.now(timezone.utc).astimezone()
+      now = datetime.now(timezone.utc).astimezone()
+      span = trace.get_current_span()
+      span.set_attribute("app.now", now.isoformat())
 
-    if search_term == None or len(search_term) < 1:
-      model['errors'] = ['search_term_blank']
-    else:
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': now.isoformat()
-      }]
-      model['data'] = results
-    return model
+      if search_term == None or len(search_term) < 1:
+        model['errors'] = ['search_term_blank']
+      else:
+        results = [{
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Andrew Brown',
+          'message': 'Cloud is fun!',
+          'created_at': now.isoformat()
+        }]
+        model['data'] = results
+      return model
