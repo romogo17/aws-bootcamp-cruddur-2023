7344fb800beef6904dbf6b0878dfde9b6ca9325c
Roberto Mora <romogo17@gmail.com>
2023-03-04 14:44:48 +0000

    instrumented user_activities

--- backend-flask/services/user_activities.py
+++ backend-flask/services/user_activities.py
@@ -1,40 +1,48 @@
 from datetime import datetime, timedelta, timezone
 from aws_xray_sdk.core import xray_recorder
 
-try:
-  xray_recorder.current_segment()
-except:
-  xray_recorder.begin_segment('user.activities')
+from opentelemetry import trace
+
+tracer = trace.get_tracer("user.activities")
+
+# try:
+#   xray_recorder.current_segment()
+# except:
+#   xray_recorder.begin_segment('user.activities')
 
 class UserActivities:
   def run(user_handle):
-    model = {
-      'errors': None,
-      'data': None
-    }
+    with tracer.start_as_current_span("user-activities-mock-data"):
+      model = {
+        'errors': None,
+        'data': None
+      }
 
-    now = datetime.now(timezone.utc).astimezone()
+      now = datetime.now(timezone.utc).astimezone()
 
-    if user_handle == None or len(user_handle) < 1:
-      model['errors'] = ['blank_user_handle']
-    else:
-      now = datetime.now()
-      results = [{
-        'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
-        'handle':  'Andrew Brown',
-        'message': 'Cloud is fun!',
-        'created_at': (now - timedelta(days=1)).isoformat(),
-        'expires_at': (now + timedelta(days=31)).isoformat()
-      }]
-      model['data'] = results
+      if user_handle == None or len(user_handle) < 1:
+        model['errors'] = ['blank_user_handle']
+      else:
+        now = datetime.now()
+        results = [{
+          'uuid': '248959df-3079-4947-b847-9e0892d1bab4',
+          'handle':  'Andrew Brown',
+          'message': 'Cloud is fun!',
+          'created_at': (now - timedelta(days=1)).isoformat(),
+          'expires_at': (now + timedelta(days=31)).isoformat()
+        }]
+        model['data'] = results
 
-    # xray ---
-    subsegment = xray_recorder.begin_subsegment('user-activities-mock-data')
-    metadata = {
-      "app.now": now.isoformat(),
-      "app.result_length": len(model['data'])
-    }
-    subsegment.put_metadata('key', metadata, 'namespace')
-    xray_recorder.end_subsegment()
+      # xray ---
+      # subsegment = xray_recorder.begin_subsegment('user-activities-mock-data')
+      # metadata = {
+      #   "app.now": now.isoformat(),
+      #   "app.result_length": len(model['data'])
+      # }
+      # subsegment.put_metadata('key', metadata, 'namespace')
+      # xray_recorder.end_subsegment()
 
-    return model
+      span = trace.get_current_span()
+      span.set_attribute("app.user_handle", user_handle)
+
+      return model
