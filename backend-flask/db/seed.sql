403ac82c9308f0c79361d8d1820226fddce21a26
Roberto Mora <romogo17@gmail.com>
2023-03-22 02:26:17 +0000

    added additional user to the postgresql seed data

--- backend-flask/db/seed.sql
+++ backend-flask/db/seed.sql
@@ -1,7 +1,8 @@
 -- this file was manually created
 INSERT INTO public.users (display_name, email, handle, cognito_user_id)
 VALUES
-  ('Roberto Mora', 'romogo17@gmail.com', 'romogo17' ,'f6ca8bff-4cfa-466f-87bf-586ee222c10a');
+  ('Roberto Mora', 'romogo17@gmail.com', 'romogo17' ,'f6ca8bff-4cfa-466f-87bf-586ee222c10a'),
+  ('Stephanie Gomez', 'romogo17+chichi@gmail.com', 'chichi' ,'f2d791f1-a54f-414e-99fc-ee4f053489cf');
 
 INSERT INTO public.activities (user_uuid, message, expires_at)
 VALUES
