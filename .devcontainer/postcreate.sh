f84e8eb729b5b164ba97d71209ac43b32052cd61
Roberto Mora <romogo17@gmail.com>
2023-03-23 13:45:34 +0000

    moving to codespaces - devcontainer fixes around create script

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
-cd $CODESPACE_VSCODE_FOLDER/frontend-react-js && npm i
-cd $CODESPACE_VSCODE_FOLDER/cognito-authz && npm i
-cd $CODESPACE_VSCODE_FOLDER/backend-flask && pip install -r requirements.tx
+nvm install v16
+
+cd ${CODESPACE_VSCODE_FOLDER}/frontend-react-js && npm i
+cd ${CODESPACE_VSCODE_FOLDER}/cognito-authz && npm i
+cd ${CODESPACE_VSCODE_FOLDER}/backend-flask && pip install -r requirements.tx
