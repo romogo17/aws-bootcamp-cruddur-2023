63f054a3918447f5f36ea76c22b9473d70dfa2c0
Roberto Mora <romogo17@gmail.com>
2023-03-23 14:06:56 +0000

    moving to codespaces - fix typo in script

--- .devcontainer/postcreate.sh
+++ .devcontainer/postcreate.sh
@@ -4,4 +4,4 @@ nvm install v16
 
 pushd frontend-react-js && npm i && popd
 pushd cognito-authz && npm i && popd
-pushd backend-flask && pip install -r requirements.tx && popd
+pushd backend-flask && pip install -r requirements.txt && popd
