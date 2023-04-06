47760ebe4601fc1cff77bc8bb759b0b46426aac2
Roberto Mora <romogo17@gmail.com>
2023-04-05 20:37:44 -0600

    fix: should return empty json when there's nothing to return

--- backend-flask/lib/db.py
+++ backend-flask/lib/db.py
@@ -93,7 +93,7 @@ class Db:
                 cur.execute(wrapped_sql, params)
                 json = cur.fetchone()
                 if json is None:
-                    "{}"
+                    return "{}"
                 else:
                     return json[0]
 
