74d1293881583811227a7d850748d18c8eb3e7c9
Roberto Mora <romogo17@gmail.com>
2023-04-05 19:50:26 -0600

    updated billing alarm and budget

--- infrastructure/00-billing-budgets-and-health/main.tf
+++ infrastructure/00-billing-budgets-and-health/main.tf
@@ -28,13 +28,13 @@ resource "aws_cloudwatch_metric_alarm" "billing_alarm" {
 resource "aws_budgets_budget" "min_spend" {
   name         = "min-spend-budget"
   budget_type  = "COST"
-  limit_amount = "2"
+  limit_amount = "10"
   limit_unit   = "USD"
   time_unit    = "MONTHLY"
 
   notification {
     comparison_operator        = "GREATER_THAN"
-    threshold                  = 1
+    threshold                  = 5
     threshold_type             = "ABSOLUTE_VALUE"
     notification_type          = "ACTUAL"
     subscriber_email_addresses = [var.account_admin_email]
